c9d3633b0a0905014001c4ea041bd277eb53f281
Ritik Kumar <53653417+Ritik609@users.noreply.github.com>
2021-08-01 17:45:26 +0530

    program to detect loop

---
+++ isLoop.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next;
+    
+    
+};
+void insert(node* &head, int x)
+{
+	/* allocate node */
+	node* ptr = new node();
+
+	/* put in the data */
+	ptr->data = x;
+    ptr->next=NULL;
+	if(head==NULL){
+	    head=ptr;
+	}
+	else{
+	    node *temp=head;
+	    while(temp->next){
+	        temp=temp->next;
+	    }
+	    temp->next=ptr;
+	    
+	}
+}
+
+void isLoop(node *start){
+    node *slow,*fast;
+    slow=start;
+    fast=start;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            std::cout << "True" << std::endl;
+            return;
+        }
+   
+    }
+    cout<<"False";
+    return;
+}
+void traversal(node *start){
+    node *temp=start;
+    while(temp!=NULL){
+        std::cout << temp->data << "->";
+        temp=temp->next;
+    }
+    
+}
+int main() {
+    node *start=NULL;
+    insert(start,1);
+	insert(start,2);
+	insert(start,3);
+	insert(start,4);
+	insert(start,5);
+   // node *temp=start;
+    //temp=temp->next->next;
+    //temp->next->next=temp;
+    isLoop(start);
+	return 0;
+}

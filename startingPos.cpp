14fc6a5b07bc57a4bc551424012d0bbd32c9a0f4
Ritik Kumar <53653417+Ritik609@users.noreply.github.com>
2021-08-02 10:46:55 +0530

    program to find starting position of the loop

---
+++ startingPos.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next;
+    
+    
+};
+void insert(node* &head, int x)
+{
+	/* allocate node */
+	node* ptr = new node();
+
+	/* put in the data */
+	ptr->data = x;
+    ptr->next=NULL;
+	if(head==NULL){
+	    head=ptr;
+	}
+	else{
+	    node *temp=head;
+	    while(temp->next){
+	        temp=temp->next;
+	    }
+	    temp->next=ptr;
+	    
+	}
+}
+
+node  *startingPoint(node *start){
+    node *slow,*fast;
+    slow=start;
+    fast=start;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            break;
+        }
+   
+    }
+   slow=start;
+   while(slow->next!=fast->next){
+       slow=slow->next;
+       fast=fast->next;
+   }
+   fast=fast->next;
+   if(fast) return fast;
+    else{
+        cout<<"Loop does not present";
+        return NULL;
+    }
+
+}
+void isLoop(node *start){
+    node *slow,*fast;
+    slow=start;
+    fast=start;
+    while(fast and fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            std::cout << "True" << std::endl;
+            return;
+        }
+   
+    }
+    cout<<"False";
+    return;
+}
+void traversal(node *start){
+    node *temp=start;
+    while(temp!=NULL){
+        std::cout << temp->data << "->";
+        temp=temp->next;
+    }
+    
+}
+int main() {
+    node *start=NULL;
+    insert(start,1);
+	insert(start,2);
+	insert(start,3);
+	insert(start,4);
+	insert(start,5);
+    node *temp=start;
+    temp=temp->next->next;
+    temp->next->next->next=temp;
+    node *ans=startingPoint(start);
+    cout<<ans->data;
+    //isLoop(start);
+	return 0;
+}
